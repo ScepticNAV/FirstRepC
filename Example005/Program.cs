6c0a1730322f9fdddb3217bf459f28943020777c
Alexander Nuykin <scepticnav@gmail.com>
2022-05-11 23:37:48 +0300

    Add Example005 If/Else

---
+++ Example005/Program.cs
@@ -0,0 +1,19 @@
+﻿// Приветствие конкретного пользователя, например: Elvis
+
+Console.WriteLine("Введите ваше имя");
+
+string username = Console.ReadLine();
+
+if(username.ToLower() == "elvis")
+
+{
+    Console.WriteLine("YoooHoo! It's Elvis!");
+}
+    
+    else
+
+    {
+        Console.Write("Hello,");
+        Console.Write(username);
+        Console.WriteLine("!");
+    }
