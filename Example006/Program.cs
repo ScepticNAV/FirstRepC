8e2f029b55f46640126f46d8cc8836f3788876c0
Alexander Nuykin <scepticnav@gmail.com>
2022-05-11 23:54:47 +0300

    Add Example006 Find max

---
+++ Example006/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача по поиску самой тажелой гири
+int a = new Random().Next(1,20);
+
+int b = new Random().Next(1,20);
+
+int c = new Random().Next(1,20);
+
+int d = new Random().Next(1,20);
+
+int e = new Random().Next(1,20);
+
+int max = a;
+
+if (a > max) max = a;
+
+if (b > max) max = b;
+
+if (c > max) max = c;
+
+if (d > max) max = d;
+
+if (e > max) max = e;
+
+Console.Write("a = ");
+Console.WriteLine(a);
+
+Console.Write("b = ");
+Console.WriteLine(b);
+
+Console.Write("c = ");
+Console.WriteLine(c);
+
+Console.Write("d = ");
+Console.WriteLine(d);
+
+Console.Write("e = ");
+Console.WriteLine(e);
+
+Console.Write("max = ");
+Console.WriteLine(max);
